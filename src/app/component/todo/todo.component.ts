16f142e626ff4ca8ccc3619b90a2a59813604c9e
Pratik Kumar Sahoo <sahoopratik22@gmail.com>
2023-07-16 19:02:29 +0530

    packages added

---
+++ src/app/component/todo/todo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
